8be020ebb1583355e606cabaf767867861232166
will-souza97 <rzy.x97@gmail.com>
2022-03-20 23:24:09 -0300

    refactor: add new project structure and refactories others

--- src/core/domain/entity.ts
+++ src/core/domain/entity.ts
@@ -4,6 +4,10 @@ export abstract class Entity<T> {
   protected readonly _id: string;
   public readonly props: T;
 
+  get id() {
+    return this._id;
+  }
+
   constructor(props: T, id?: string) {
     this._id = id ?? uuid();
     this.props = props;
