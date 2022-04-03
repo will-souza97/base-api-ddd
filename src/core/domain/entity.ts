e22a0c02d6698522154ddfea8df99f95bad8b3b6
will-souza97 <rzy.x97@gmail.com>
2022-04-03 14:29:46 -0300

    refactor: add and update returns logics

--- src/core/domain/entity.ts
+++ src/core/domain/entity.ts
@@ -2,7 +2,7 @@ import { v4 as uuid } from 'uuid';
 
 export abstract class Entity<T> {
   protected readonly _id: string;
-  public readonly props: T;
+  protected readonly props: T;
 
   get id() {
     return this._id;
