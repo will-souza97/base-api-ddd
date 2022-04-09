2519598e56dc0aae94357f05e548e93b2230ab5e
William de Souza <54961000+will-souza97@users.noreply.github.com>
2022-04-09 00:24:14 -0300

    Delete application directory

--- application/repositories/account-repository.ts
+++
@@ -1,7 +0,0 @@
-import { Account } from '@domain/entities/account';
-import { Email } from '@domain/values-objects/email';
-
-export interface AccountRepository {
-  create(data: Account): Promise<void>;
-  existsEmail(email: Email): Promise<boolean>;
-}
--- application/usecases/errors/existing-email.ts
+++
@@ -1,8 +0,0 @@
-import { UseCaseError } from '@core/domain/errors/use-case-error';
-
-export class ExistingEmailError extends Error implements UseCaseError {
-  constructor(email: string) {
-    super(`Email: ${email} is already in use`);
-    this.name = 'ExistingEmailError';
-  }
-}
