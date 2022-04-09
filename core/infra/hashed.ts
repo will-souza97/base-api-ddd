cefd1aa462de0ed83e602cd87f3b3f249f96dd8e
will-souza97 <rzy.x97@gmail.com>
2022-04-09 01:26:49 -0300

    fix: fix bug

--- core/infra/hashed.ts
+++
@@ -1,4 +0,0 @@
-export interface Hashed {
-  generate(payload: string): Promise<string>;
-  compare(payload: string, hash: string): Promise<boolean>;
-}
--- domain/values-objects/errors/invalid-email-error.ts
+++
@@ -1,8 +0,0 @@
-import { DomainError } from '@core/domain/errors/domain-error';
-
-export class InvalidEmailError extends Error implements DomainError {
-  constructor(email: string) {
-    super(`Email: ${email} is not in a valid format`);
-    this.name = 'InvalidEmailError';
-  }
-}
